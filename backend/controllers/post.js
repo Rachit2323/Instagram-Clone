fabeda2e065661327b28ab8bd8ef4febf8654544
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2023-08-16 23:16:42 +0530

    Server updated

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Post=require('../models/post');
-const authMiddleware=require('../controllers/authMiddleware.js'); 
+
+const Story = require("../models/story.js");
 
 exports.createPost=async(req,res)=>{
     try{
@@ -30,7 +31,18 @@ exports.createPost=async(req,res)=>{
 
 exports.createStory=async(req,res)=>{
     try{
+        const {title,description,tags}=req.body;
+        const tagsArray = tags ? tags : [];
+        console.log(title,description,tagsArray);
+        const newPost = new Story({
+            title,
+            description,
+            userId: req.userId
+        });
+      
+          await newPost.save();
 
+        res.status(200).json({ error: "Story created succesfully" });
     }catch(error)
     {
         console.log(error);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,5 +4,7 @@ const homeController = require('../controllers/post.js');
 const authMiddleware=require('../controllers/authMiddleware.js'); 
 
 app.post("/createpost", authMiddleware,homeController.createPost);
+app.post("/createstory", authMiddleware,homeController.createStory);
+
 
 module.exports = app;
