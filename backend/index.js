1d68d89d25bd6ed0e8a1186aa2be6e61fc133f79
karan <karanbisht511@gmail.com>
2023-08-22 23:07:20 +0530

    Putting mongodb connection code in a separate file

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectMongoDb=require('./database/connectMongoDb')
+
 const app = express();
+
 const userRoutes = require("./routes/user.js");
 const postRoutes=require("./routes/post.js");
 
 app.use(express.json());
 
+connectMongoDb()
+
 app.use("/users", userRoutes); 
 app.use("/post", postRoutes); 
-
-mongoose.connect("mongodb://localhost:27017/allthree", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("Connected to MongoDB");
-}).catch(err => {
-  console.error("MongoDB connection error:", err);
-});
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
