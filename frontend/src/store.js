de6420a834b2b2dd958bb8ce5c3e88556bf66846
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2023-08-24 19:46:55 +0530

    front store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,14 @@
 
 
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './Reducers/auth'; // Make sure to provide the correct path
+import authReducer from './Reducers/auth'; 
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    // You can add more reducers here if needed
+  
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(/* any additional middleware you want */),
+
 });
 
 export default store;
