c85efb929bcc4a8c2b5883a9907096f4de071e8b
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2023-08-27 18:03:08 +0530

    signup update

--- frontend/src/Components/user/Test.js
+++ frontend/src/Components/user/Test.js
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from "react";
-import {
-  AiOutlineBulb,
-  AiOutlineEye,
-  AiOutlineEyeInvisible,
-} from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineBulb, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import "./Test.css";
 import { motion } from "framer-motion";
 
 const Test = () => {
-  const [isActive, setIsActive] = React.useState(false);
+  const [isCardFlipped, setIsCardFlipped] = useState(false);
   const [auth, isAuth] = useState(true);
-
   const [showPassword, setShowPassword] = useState(true);
+  const [passowrdhintshow,setPasswordHintShow]=useState(false);
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -21,8 +17,14 @@ const Test = () => {
     information: "",
     password: "",
   });
-  const handleCardFlip = () => {
-    setIsActive(!isActive);
+
+  const handleCardToggle = () => {
+    isAuth(!auth);
+    setIsCardFlipped(!isCardFlipped);
+  };
+
+  const passwordHint = () => {
+    setPasswordHintShow((prev) => !prev); 
   };
 
   const handleChange = (e) => {
@@ -45,19 +47,18 @@ const Test = () => {
     <div className="signup_container_outer">
       <div className="sigup_wrapper_background-image">
         <div className="outer_wrapper_frame_signup_01">
-          <div className="outer_wrapper_frame_signup_01_left">
-            <span>Welcome to The Think Tribe</span>
-            <p>A place to share knowledge and better understand the world.</p>
-          </div>
+        <div className="outer_wrapper_frame_signup_01_left">
+           <span>Welcome to The Think Tribe</span>
+             <p>A place to share knowledge and better understand the world.</p>
+           </div>
           <div className="outer_wrapper_frame_signup_01_right_00">
             <motion.div
-              className={`card ${isActive ? "active" : ""}`}
-              onClick={handleCardFlip}
+              className={`card ${isCardFlipped ? "active" : ""}`}
               initial={{ rotateY: 0 }}
-              animate={{ rotateY: isActive ? 180 : 0 }}
+              animate={{ rotateY: isCardFlipped ? 180 : 0 }}
               transition={{ duration: 0.5 }}
             >
-              <div className={`outer_wrapper_frame_signup_01_right ${isActive ? "mirrored-content" : ""}`}>
+              <div className={`outer_wrapper_frame_signup_01_right ${isCardFlipped ? "mirrored-content" : ""}`}>
                 <section>
                   <span>{auth ? "Sign Up" : "Sign in"}</span>
                   <section>
@@ -126,15 +127,19 @@ const Test = () => {
                       </div>
                     </div>
                   </section>
-                  <div className="outer_wrapper_frame_signup_01_right_01">
+                  <div className="outer_wrapper_frame_signup_01_right_01" onClick={passwordHint}>
                     <AiOutlineBulb /> Password hint
+                   
                   </div>
+
+                  {passowrdhintshow&&<div className="outer_wrapper_frame_signup_01_right_01_text_below">Password should be more than 8 Characters , with 1 special character and 1 uppercase letter</div>}
+
                   <button>{auth ? "Sign Up" : "Sign in"}</button>
                   <p>
                     {auth
                       ? "Already have an account?"
                       : "Don't have an account!"}{" "}
-                     <span onClick={() => isAuth(!auth)}>
+                     <span onClick={handleCardToggle}>
                       {auth ? "Sign in" : "Sign up"}
                     </span>
                   </p>
